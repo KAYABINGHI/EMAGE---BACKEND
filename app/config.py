fc343fe55dd3cb2ae84d3c25c7cd98e69c9959d2
BKiptoo <bettsonk@gmail.com>
2025-10-21 18:24:11 +0300

    Initialize Flask application with JWT and database configuration
    - Created app initialization in `app/__init__.py` with Flask, SQLAlchemy, JWT, and CORS.
    - Added configuration settings in `app/config.py` for environment variables.
    - Implemented main application entry point in `main.py`.
    - Set up Alembic migrations with necessary configuration files.
    - Removed outdated division documentation and requirements files.

---
+++ app/config.py
@@ -0,0 +1,19 @@
+# app/config.py
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+class Config:
+    # General Flask settings
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+
+    # Database configuration
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', 'False').lower() == 'true'
+
+    # JWT settings
+    JWT_ACCESS_TOKEN_EXPIRES = os.getenv('JWT_ACCESS_TOKEN_EXPIRES', 'False').lower() == 'true'
+    JWT_IDENTITY_CLAIM = os.getenv('JWT_IDENTITY_CLAIM')
---
+++ main.py
@@ -0,0 +1,16 @@
+from app import create_app
+import os
+from dotenv import load_dotenv
+from flask_cors import CORS
+load_dotenv()
+
+app = create_app()
+CORS(app)
+
+# @app.route("/")
+def home():
+    return "Welcome to the Student Management System"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
