b2923b2e083236ce31e3fdc6c19098ab4694f844
moen <moenr49@gmail.com>
2025-11-12 15:27:44 +0300

    backend intergration

--- main.py
+++ main.py
@@ -43,4 +43,4 @@ if __name__ == "__main__":
     app = create_app()
     with app.app_context():
         db.create_all()  # create tables if not exist
-    app.run(debug=True,port=5501)
+    app.run(host='0.0.0.0',debug=True,port=5501)
